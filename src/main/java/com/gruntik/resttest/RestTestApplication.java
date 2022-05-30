11a46a28ecf09a323a55f9ff354858c4ab66cc61
gruntik <gruntov99@mail.ru>
2022-05-30 11:01:28 +0300

    check version 66

--- src/main/java/com/gruntik/resttest/RestTestApplication.java
+++ src/main/java/com/gruntik/resttest/RestTestApplication.java
@@ -10,7 +10,7 @@ public class RestTestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RestTestApplication.class, args);
-        System.out.println("Message for test actions");
+        System.out.println("Message for test actions 1");
     }
 
 }
